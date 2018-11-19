25c62a9a93278499056efa3393918871badd29c3
MrHidalgo <Luckyhidalgo@gmail.com>
2018-11-19 14:27:59 +0200

    Swiper js - refactor code.

--- dest/js/app.js
+++ dest/js/app.js
@@ -84,329 +84,119 @@ var initSvg4everybody = function initSvg4everybody() {
  * @description initialize Swiper
  */
 var initSwiper = function initSwiper() {
-  var mySwiperFinding = new Swiper('.swiper-container--finding', {
 
-    // Optional parameters
-    wrapperClass: "swiper-wrapper",
-    slideClass: "swiper-slide",
-    direction: 'horizontal', // 'horizontal' or 'vertical'
-    loop: true,
-    watchOverflow: true,
-    normalizeSlideIndex: true,
+  /**
+   *
+   * @returns {{wrapperClass: string, slideClass: string, direction: string, loop: boolean, watchOverflow: boolean, normalizeSlideIndex: boolean, grabCursor: boolean, freeMode: boolean, effect: string, fadeEffect: {crossFade: boolean}, touchMoveStopPropagation: boolean, simulateTouch: boolean, allowSwipeToNext: boolean, allowSwipeToPrev: boolean, allowPageScroll: string, slidesPerView: number, spaceBetween: number, pagination: {el: string, clickable: boolean}, navigation: {nextEl: string, prevEl: string}, on: {init: on.init}}}
+   */
+  var swiperOption = function swiperOption() {
+    return {
+      wrapperClass: "swiper-wrapper",
+      slideClass: "swiper-slide",
+      direction: 'horizontal', // 'horizontal' or 'vertical'
+      loop: true,
+      watchOverflow: true,
+      normalizeSlideIndex: true,
 
-    grabCursor: false,
-    freeMode: false,
+      grabCursor: false,
+      freeMode: false,
 
-    effect: 'fade', // "slide", "fade", "cube", "coverflow" or "flip"
-    fadeEffect: {
-      crossFade: true
-    },
+      effect: 'fade', // "slide", "fade", "cube", "coverflow" or "flip"
+      fadeEffect: {
+        crossFade: true
+      },
 
-    // off touch for destop
-    touchMoveStopPropagation: false,
-    simulateTouch: false,
-    allowSwipeToNext: true,
-    allowSwipeToPrev: true,
-    allowPageScroll: "auto ",
+      // off touch for destop
+      touchMoveStopPropagation: false,
+      simulateTouch: false,
+      allowSwipeToNext: true,
+      allowSwipeToPrev: true,
+      allowPageScroll: "auto ",
 
-    slidesPerView: 1,
-    spaceBetween: 0,
+      slidesPerView: 1,
+      spaceBetween: 0,
 
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true
-    },
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+      },
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.slider__button-next',
-      prevEl: '.slider__button-prev'
-    },
-    on: {
-      init: function init() {
-        var slider = this.$el,
-            textContainer = slider.find('.slider__text-wrapper'),
-            paginationContainer = slider.find('.slider__pagination');
+      // Navigation arrows
+      navigation: {
+        nextEl: '.slider__button-next',
+        prevEl: '.slider__button-prev'
+      },
+      on: {
+        init: function init() {
+          var slider = this.$el,
+              textContainer = slider.find('.slider__text-wrapper'),
+              paginationContainer = slider.find('.slider__pagination');
 
-        var maxHeight = 0;
+          var maxHeight = 0;
 
-        var _iteratorNormalCompletion = true;
-        var _didIteratorError = false;
-        var _iteratorError = undefined;
+          var _iteratorNormalCompletion = true;
+          var _didIteratorError = false;
+          var _iteratorError = undefined;
 
-        try {
-          for (var _iterator = $(textContainer)[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-            var el = _step.value;
-
-            if ($(el).outerHeight(true) > maxHeight) {
-              maxHeight = $(el).outerHeight(true);
-            }
-          }
-        } catch (err) {
-          _didIteratorError = true;
-          _iteratorError = err;
-        } finally {
           try {
-            if (!_iteratorNormalCompletion && _iterator.return) {
-              _iterator.return();
+            for (var _iterator = $(textContainer)[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+              var el = _step.value;
+
+              if ($(el).outerHeight(true) > maxHeight) {
+                maxHeight = $(el).outerHeight(true);
+              }
             }
+          } catch (err) {
+            _didIteratorError = true;
+            _iteratorError = err;
           } finally {
-            if (_didIteratorError) {
-              throw _iteratorError;
+            try {
+              if (!_iteratorNormalCompletion && _iterator.return) {
+                _iterator.return();
+              }
+            } finally {
+              if (_didIteratorError) {
+                throw _iteratorError;
+              }
             }
           }
+
+          $(paginationContainer).css({
+            'top': maxHeight
+          });
         }
-
-        $(paginationContainer).css({
-          'top': maxHeight
-        });
       }
-    }
-  });
+    };
+  };
 
-  var mySwiperSave = new Swiper('.swiper-container--save', {
+  /**
+   * @name mySwiperFinding
+   *
+   * @description
+   */
+  var mySwiperFinding = new Swiper('.swiper-container--finding', swiperOption());
 
-    // Optional parameters
-    wrapperClass: "swiper-wrapper",
-    slideClass: "swiper-slide",
-    direction: 'horizontal', // 'horizontal' or 'vertical'
-    loop: true,
-    watchOverflow: true,
-    normalizeSlideIndex: true,
+  /**
+   * @name mySwiperSave
+   *
+   * @description
+   */
+  var mySwiperSave = new Swiper('.swiper-container--save', swiperOption());
 
-    grabCursor: false,
-    freeMode: false,
+  /**
+   * @name mySwiperManage
+   *
+   * @description
+   */
+  var mySwiperManage = new Swiper('.swiper-container--manage', swiperOption());
 
-    effect: 'fade', // "slide", "fade", "cube", "coverflow" or "flip"
-    fadeEffect: {
-      crossFade: true
-    },
-
-    // off touch for destop
-    touchMoveStopPropagation: false,
-    simulateTouch: false,
-    allowSwipeToNext: true,
-    allowSwipeToPrev: true,
-    allowPageScroll: "auto ",
-
-    slidesPerView: 1,
-    spaceBetween: 0,
-
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true
-    },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: '.slider__button-next',
-      prevEl: '.slider__button-prev'
-    },
-    on: {
-      init: function init() {
-        var slider = this.$el,
-            textContainer = slider.find('.slider__text-wrapper'),
-            paginationContainer = slider.find('.slider__pagination');
-
-        var maxHeight = 0;
-
-        var _iteratorNormalCompletion2 = true;
-        var _didIteratorError2 = false;
-        var _iteratorError2 = undefined;
-
-        try {
-          for (var _iterator2 = $(textContainer)[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-            var el = _step2.value;
-
-            if ($(el).outerHeight(true) > maxHeight) {
-              maxHeight = $(el).outerHeight(true);
-            }
-          }
-        } catch (err) {
-          _didIteratorError2 = true;
-          _iteratorError2 = err;
-        } finally {
-          try {
-            if (!_iteratorNormalCompletion2 && _iterator2.return) {
-              _iterator2.return();
-            }
-          } finally {
-            if (_didIteratorError2) {
-              throw _iteratorError2;
-            }
-          }
-        }
-
-        $(paginationContainer).css({
-          'top': maxHeight
-        });
-      }
-    }
-  });
-
-  var mySwiperManage = new Swiper('.swiper-container--manage', {
-
-    // Optional parameters
-    wrapperClass: "swiper-wrapper",
-    slideClass: "swiper-slide",
-    direction: 'horizontal', // 'horizontal' or 'vertical'
-    loop: true,
-    watchOverflow: true,
-    normalizeSlideIndex: true,
-
-    grabCursor: false,
-    freeMode: false,
-
-    effect: 'fade', // "slide", "fade", "cube", "coverflow" or "flip"
-    fadeEffect: {
-      crossFade: true
-    },
-
-    // off touch for destop
-    touchMoveStopPropagation: false,
-    simulateTouch: false,
-    allowSwipeToNext: true,
-    allowSwipeToPrev: true,
-    allowPageScroll: "auto ",
-
-    slidesPerView: 1,
-    spaceBetween: 0,
-
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true
-    },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: '.slider__button-next',
-      prevEl: '.slider__button-prev'
-    },
-    on: {
-      init: function init() {
-        var slider = this.$el,
-            textContainer = slider.find('.slider__text-wrapper'),
-            paginationContainer = slider.find('.slider__pagination');
-
-        var maxHeight = 0;
-
-        var _iteratorNormalCompletion3 = true;
-        var _didIteratorError3 = false;
-        var _iteratorError3 = undefined;
-
-        try {
-          for (var _iterator3 = $(textContainer)[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
-            var el = _step3.value;
-
-            if ($(el).outerHeight(true) > maxHeight) {
-              maxHeight = $(el).outerHeight(true);
-            }
-          }
-        } catch (err) {
-          _didIteratorError3 = true;
-          _iteratorError3 = err;
-        } finally {
-          try {
-            if (!_iteratorNormalCompletion3 && _iterator3.return) {
-              _iterator3.return();
-            }
-          } finally {
-            if (_didIteratorError3) {
-              throw _iteratorError3;
-            }
-          }
-        }
-
-        $(paginationContainer).css({
-          'top': maxHeight
-        });
-      }
-    }
-  });
-
-  var mySwiperControl = new Swiper('.swiper-container--control', {
-
-    // Optional parameters
-    wrapperClass: "swiper-wrapper",
-    slideClass: "swiper-slide",
-    direction: 'horizontal', // 'horizontal' or 'vertical'
-    loop: true,
-    watchOverflow: true,
-    normalizeSlideIndex: true,
-
-    grabCursor: false,
-    freeMode: false,
-
-    effect: 'fade', // "slide", "fade", "cube", "coverflow" or "flip"
-    fadeEffect: {
-      crossFade: true
-    },
-
-    // off touch for destop
-    touchMoveStopPropagation: false,
-    simulateTouch: false,
-    allowSwipeToNext: true,
-    allowSwipeToPrev: true,
-    allowPageScroll: "auto ",
-
-    slidesPerView: 1,
-    spaceBetween: 0,
-
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true
-    },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: '.slider__button-next',
-      prevEl: '.slider__button-prev'
-    },
-    on: {
-      init: function init() {
-        var slider = this.$el,
-            textContainer = slider.find('.slider__text-wrapper'),
-            paginationContainer = slider.find('.slider__pagination');
-
-        var maxHeight = 0;
-
-        var _iteratorNormalCompletion4 = true;
-        var _didIteratorError4 = false;
-        var _iteratorError4 = undefined;
-
-        try {
-          for (var _iterator4 = $(textContainer)[Symbol.iterator](), _step4; !(_iteratorNormalCompletion4 = (_step4 = _iterator4.next()).done); _iteratorNormalCompletion4 = true) {
-            var el = _step4.value;
-
-            if ($(el).outerHeight(true) > maxHeight) {
-              maxHeight = $(el).outerHeight(true);
-            }
-          }
-        } catch (err) {
-          _didIteratorError4 = true;
-          _iteratorError4 = err;
-        } finally {
-          try {
-            if (!_iteratorNormalCompletion4 && _iterator4.return) {
-              _iterator4.return();
-            }
-          } finally {
-            if (_didIteratorError4) {
-              throw _iteratorError4;
-            }
-          }
-        }
-
-        $(paginationContainer).css({
-          'top': maxHeight
-        });
-      }
-    }
-  });
+  /**
+   * @name mySwiperControl
+   *
+   * @description
+   */
+  var mySwiperControl = new Swiper('.swiper-container--control', swiperOption());
 };
 
 /**
