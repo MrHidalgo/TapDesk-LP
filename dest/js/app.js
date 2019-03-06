d2caf6641b41e257aa66b48d94535256e00269e2
MrHidalgo <Luckyhidalgo@gmail.com>
2019-03-06 10:01:25 +0200

    Create resources inner page, add popup video.

--- dest/js/app.js
+++ dest/js/app.js
@@ -54,6 +54,30 @@ var initHeaderFixed = function initHeaderFixed() {
 };
 
 /**
+ *
+ */
+var initPopups = function initPopups() {
+
+  $('[popup-js]').magnificPopup({
+    type: 'iframe',
+    fixedContentPos: true,
+    fixedBgPos: true,
+    overflowY: 'auto',
+    closeBtnInside: true,
+    preloader: false,
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'is-show',
+    callbacks: {
+      beforeOpen: function beforeOpen() {
+        this.st.mainClass = this.st.el.attr('data-effect');
+      },
+      close: function close() {}
+    }
+  });
+};
+
+/**
  * @name initPreventBehavior
  * @description
  */
@@ -815,6 +839,7 @@ $(document).ready(function (ev) {
     initStellar();
     initViewPortChecker();
     initSmoothScroll();
+    initPopups();
 
     // callback
     initSelect();
